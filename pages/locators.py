45f50dd1c3f201f0997141d82b07e32d13d84eb0
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-03-10 23:39:39 +0300

    Переходы между страницами.

--- pages/locators.py
+++ pages/locators.py
@@ -6,5 +6,5 @@ class MainPageLocators:
 
 
 class LoginPageLocators:
-    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form_")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form_")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
--- pages/login_page.py
+++ pages/login_page.py
@@ -10,7 +10,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # проверка на корректный url адрес
-        assert 'login' in self.browser.current_url(), "Invalid url"
+        assert 'login' in self.browser.current_url, "Invalid url"
 
     def should_be_login_form(self):
         # проверка, что есть форма логина
