f0b6be25d0c980498934c76ffa51c51eda8e0e5e
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-03-11 19:41:45 +0300

    Добавление в корзину со страницы товара.

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    BTN_ADD_TO_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, 'p.price_color')
+    MESSAGE_PRODUCT_NAME = (By.CSS_SELECTOR, '#messages strong')
+    MESSAGE_PRODUCT_PRICE = (By.CSS_SELECTOR, '#messages p strong')
---
+++ pages/product_page.py
@@ -0,0 +1,24 @@
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        self.browser.find_element(*ProductPageLocators.BTN_ADD_TO_BASKET).click()
+
+    def get_element_text(self, how, what: str):
+        if self.is_element_present(how, what):
+            return self.browser.find_element(how, what).text
+        return None
+
+    def match_product_name(self):
+        # проверка, совпадения имён
+        mes_name = self.get_element_text(*ProductPageLocators.MESSAGE_PRODUCT_NAME)
+        name = self.get_element_text(*ProductPageLocators.PRODUCT_NAME)
+        assert mes_name == name, "The names don't match"
+
+    def match_product_price(self):
+        # проверка, совпадения цен
+        mes_price = self.get_element_text(*ProductPageLocators.MESSAGE_PRODUCT_PRICE)
+        price = self.get_element_text(*ProductPageLocators.PRODUCT_PRICE)
+        assert mes_price == price, "The prices don't match"
