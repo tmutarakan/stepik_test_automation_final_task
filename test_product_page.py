0887fea23a98ac7cc436ea8c01f5c43fecd3fb31
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-03-13 08:55:54 +0300

    Исправлена ссылка.

--- test_product_page.py
+++ test_product_page.py
@@ -7,7 +7,7 @@ import pytest
 
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
